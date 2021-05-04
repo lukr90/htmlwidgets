15b923157335b4d1fd5f32bc6ec7710736c0b533
Lucas Kramer <lucas.kramer@hunter.de>
2021-05-04 16:42:06 +0200

    added area/spline chart

---
+++ R/C3AreaChart_perc.R
@@ -0,0 +1,53 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+C3AreaChart_perc <- function(dataset, colors, width = NULL, height = NULL, elementId = NULL) {
+  
+  # forward options using x
+  x = list(
+    dataset  = dataset,
+    colors   = colors
+  )
+  
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'C3AreaChart_perc',
+    x,
+    width = width,
+    height = height,
+    package = 'C3',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for C3AreaChart_perc
+#'
+#' Output and render functions for using C3AreaChart_perc within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a C3AreaChart_perc
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name C3AreaChart_perc-shiny
+#'
+#' @export
+C3AreaChart_percOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'C3AreaChart_perc', width, height, package = 'C3')
+}
+
+#' @rdname C3AreaChart_perc-shiny
+#' @export
+renderC3AreaChart_perc <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, C3AreaChart_percOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/C3AreaChart_perc.js
@@ -0,0 +1,132 @@
+HTMLWidgets.widget({
+
+  name: 'C3AreaChart_perc',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+    
+    
+    // create custom function for week of year
+    
+    function getWeek(title){
+      const today = title;
+      const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+      const pastDaysOfYear = (today - firstDayOfYear) / 86400000;
+      return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7) - 1;
+    }
+
+    // create an empty chart
+   var chart = null;
+   
+   var ger = d3.locale ({
+            "decimal": ".",
+            "thousands": ",",
+            "grouping": [3],
+            "currency": ["", "€"],
+            "dateTime": "%a %b %e %X %Y",
+            "date": "%m/%d/%Y",
+            "time": "%H:%M:%S",
+            "periods": ["AM", "PM"],
+            "days": ["Sontag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+            "shortDays": ["Son", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+            "months": ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+            "shortMonths": ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    })
+
+    return {
+
+      renderValue: function(x) {
+
+        // if the chart does not exist, create it via c3.generate
+        if(chart===null){
+
+            keys = _.keys(x.dataset);
+
+            chart = c3.generate({
+
+              // specify the container element we want the chart to render in
+                bindto: el,
+                data: {
+
+                  // intialize with an empty array
+                    json: [],
+                    keys: {
+                          // use Time for x-axis
+                          x: "Time",
+
+                      // use the remaining data for y-values
+                        value: keys,
+                    },
+
+                    // set chart types
+                    types: {
+
+                    // default is line, we want totals to be displayed as bars
+                        Anteil: 'spline',
+                        OptimalerLagerwert: 'area',
+                        VerfuegbarerLagerwert: 'area'
+                    },
+                    axes: {
+                        Prozent: 'y2',
+                        OptimalerLagerwert: 'y',
+                        VerfuegbarerLagerwert: 'y'
+                    }
+                },
+                grid: {
+                    y: {
+                        lines: [{value: 0, class: "cc"}]
+                    }
+                },
+                axis: {
+                    x: {
+                      //  x axis as timeseries
+                        type: "timeseries",
+
+                        // tick format x-axis
+                        tick: {
+                            format: "%Y-%m"
+                        }
+                    },
+                    y: {
+                        tick: {
+                            format: ger.numberFormat("$,.2f")
+                        }
+                    },
+                    y2: {
+                        show: true,
+                        
+                        max: 1,
+                        
+                        tick: {
+                            format: d3.format(',%')
+                        }
+                    }
+                },
+                tooltip: {
+          		    // tooltip
+          		    format: {
+          		     title: function(title){return 'Monat: ' + (title.getMonth() + 1) + ', KW: ' + getWeek(title);}
+          		    }
+          		}
+            });
+        }
+
+        // at this stage the chart always exists
+        // get difference in keys
+        var old_keys = _.keys(chart.x());
+        var new_keys = _.keys(x.dataset);
+        var diff     = _.difference(old_keys,new_keys);
+
+        // update the data and colors
+        chart.load({
+          json  : x.dataset,
+          colors: x.colors,
+
+          // unload data that we don't need anymore
+          unload: diff
+        });
+      }
+    };
+  }
+});
