f9dc1c49593e30926d573253f438c1f8b4b60ba3
Lucas Kramer <lucas.kramer@hunter.de>
2021-09-21 15:01:39 +0200

    added simpleAreaChart

---
+++ R/C3simpleAreaChart.R
@@ -0,0 +1,53 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+C3simpleAreaChart <- function(dataset, colors, width = NULL, height = NULL, elementId = NULL) {
+  
+  # forward options using x
+  x = list(
+    dataset  = dataset,
+    colors   = colors
+  )
+  
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'C3simpleAreaChart',
+    x,
+    width = width,
+    height = height,
+    package = 'C3',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for C3simpleAreaChart
+#'
+#' Output and render functions for using C3simpleAreaChart within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a C3simpleAreaChart
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name C3simpleAreaChart-shiny
+#'
+#' @export
+C3AreaChart_intOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'C3simpleAreaChart', width, height, package = 'C3')
+}
+
+#' @rdname C3simpleAreaChart-shiny
+#' @export
+renderC3AreaChart_int <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, C3AreaChart_intOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/C3simpleAreaChart.js
@@ -0,0 +1,101 @@
+HTMLWidgets.widget({
+
+  name: 'C3simpleAreaChart',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+    
+    
+    // create custom function for week of year
+    
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+    // create an empty chart
+    var chart = null;
+
+    return {
+
+      renderValue: function(x) {
+
+        // if the chart does not exist, create it via c3.generate
+        if(chart===null){
+
+            keys = _.keys(x.dataset);
+
+            chart = c3.generate({
+
+              // specify the container element we want the chart to render in
+                bindto: el,
+                data: {
+
+                  // intialize with an empty array
+                    json: [],
+                    keys: {
+                          // use Time for x-axis
+                          x: "Time",
+
+                      // use the remaining data for y-values
+                        value: keys,
+                    },
+
+                    // set chart types
+                    types: {
+
+                    // default is line, we want totals to be displayed as bars
+                        GesamtForecast: 'area',
+                        GesamtVerkauf: 'area'
+                    },
+                },
+                grid: {
+                    y: {
+                        lines: [
+                          {value: 0, class: "cc"}
+                          ]
+                    },
+                    
+                    x: {
+                        lines: [
+                            {value: date, text: date, class: "cc"}
+                          ]
+                    }
+                },
+                axis: {
+                    x: {
+                      //  x axis as timeseries
+                        type: "timeseries",
+
+                        // tick format x-axis
+                        tick: {
+                            format: "%Y-%m"
+                        }
+                    },
+                },
+                tooltip: {
+          		    // tooltip
+          		    format: {
+          		     title: function(title){return 'Jahr: ' + title.getFullYear() + ', Monat: ' + (title.getMonth() + 1);}
+          		    }
+          		}
+            });
+        }
+
+        // at this stage the chart always exists
+        // get difference in keys
+        var old_keys = _.keys(chart.x());
+        var new_keys = _.keys(x.dataset);
+        var diff     = _.difference(old_keys,new_keys);
+
+        // update the data and colors
+        chart.load({
+          json  : x.dataset,
+          colors: x.colors,
+
+          // unload data that we don't need anymore
+          unload: diff
+        });
+      }
+    };
+  }
+});
